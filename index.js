0a4ab072e1b49d7b727a53204ccca0f6b519a17f
Christopher Moser <moser121@icloud.com>
2018-02-21 14:09:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (string.toLowerCase()===string);
 } else if (string.toUpperCase()===string){
   return "YES INDEED!";
 } else if (string==="I love you, Grandma.");
-return 
+return "I love you, too.";
   
 
   
